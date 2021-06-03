7522960757a079554256be97fd7843dd72f9ae97
benmrx <benmrx@yahoo.com>
2021-06-02 22:56:05 -0700

    Update MRX_Hello_World.lua

--- Editing/MRX_Hello_World.lua
+++ Editing/MRX_Hello_World.lua
@@ -1,7 +1,9 @@
 -- @description MRX Hello world
 -- @author Ben Kersten
--- @version 1.0
+-- @version 1.1
+-- @changelog
+--   This is the changelog text
 -- @about
 --   This is the initial About text
 
-reaper.ShowConsoleMsg 'MRX - Hello World!'
+reaper.ShowConsoleMsg 'MRX - Hello World! - v1.1'
